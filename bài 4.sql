1535e3f6e7bd04bbe57f5fd7cb566e57049b67f0
lkhavan2203-source <lkhavan2203@gmail.com>
2025-11-13 21:15:54 +0700

    Add files via upload

---
+++ bài 4.sql
@@ -0,0 +1,22 @@
+create database CompanyDB; 
+use CompanyDB ;
+create table Employess(
+emplayeeID int primary key auto_increment,
+firstname varchar(50),
+lastname varchar(50)
+);
+ ALTER TABLE Employess 
+ADD COLUMN HireDate DATE;
+Alter table Employess
+add column Salary INT; 
+INSERT INTO Employees (FirstName, LastName, HireDate, Salary)
+VALUES
+    ('Hoa', 'Nguyen', '2023-01-15', 18000000),
+    ('Tuan', 'Le', '2022-08-20', 25500000),
+    ('Mai', 'Pham', '2024-03-01', 12000000);
+UPDATE Salary 
+SET Salary = 4800
+Where EmployeeID = 2;
+UPDATE HireDate 
+SET HireDate = 2021-08-01 
+Where FirstName = 'David'
